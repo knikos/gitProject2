6f11fe8cb5a310e36570c79fcaae16c1a35abc9b
Kleonikos Kyriakis <nikosgr8@gmail.com>
2019-08-13 14:53:34 +0300

    first commit

---
+++ src/test/java/gitProject2/TestExampleServlet.java
@@ -0,0 +1,14 @@
+package gitProject2;
+
+import junit.framework.TestCase;
+
+/**
+ * Testing {@link gitProject2.ExampleMacro}
+ */
+public class TestExampleServlet extends TestCase
+{
+    public void testBasic()
+    {
+        // add test here...
+    }
+}
